fa9de47b6adc6b29d2b5e7347af9365bd1ec973e
seibtoy <seibtoy@gmail.com>
2025-02-11 14:04:59 +0200

    create header, add responsive design for mobile and tablets, add mixins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
