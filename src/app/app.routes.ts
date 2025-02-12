8019bac87922c0dfecb440e0e765db18b8a0a3e0
seibtoy <seibtoy@gmail.com>
2025-02-12 16:42:13 +0200

    create empty registration page, add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/HomePage/home-page.component';
+import { RegisterPageComponent } from './pages/RegisterPage/reg-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: RegisterPageComponent },
+];
